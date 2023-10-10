369e08bfefd387acaae8775a3b261b2e30bbac67
Andrew Gits <andrewgits@gmail.com>
2023-10-10 13:12:25 +0100

    fix: properly handle an empty variantImages array

--- plugin/src/domain/shared/mappers/DefaultProductMapper.spec.ts
+++ plugin/src/domain/shared/mappers/DefaultProductMapper.spec.ts
@@ -30,6 +30,15 @@ describe('map CT product to Klaviyo product', () => {
         expect(klaviyoEvent).toMatchSnapshot();
     });
 
+    it('should map commercetools product variants with an empty images array to a klaviyo variants', () => {
+        const variant = { ...structuredClone(ctGet1Product.results[0].masterData.current.masterVariant), images: [] };
+        const klaviyoEvent = productMapper.mapCtProductVariantToKlaviyoVariant(
+            ctGet1Product.results[0] as unknown as Product,
+            variant as unknown as ProductVariant,
+        );
+        expect(klaviyoEvent).toMatchSnapshot();
+    });
+
     it('should map secondary locales to appropiate custom_metadata properties', () => {
         const customProduct = {
             ...ctGet1Product.results[0],
