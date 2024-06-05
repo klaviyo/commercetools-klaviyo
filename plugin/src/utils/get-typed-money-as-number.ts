854745965cf426ef4316f63cf5c4dc68ba440c92
KthProg <kyle.thomas.hooks@gmail.com>
2024-06-05 16:50:56 -0400

    use $0.00 for price is price not defined, log info

--- plugin/src/utils/get-typed-money-as-number.ts
+++ plugin/src/utils/get-typed-money-as-number.ts
@@ -1,5 +1,10 @@
 import { TypedMoney } from '@commercetools/platform-sdk';
+import logger from './log';
 
-export const getTypedMoneyAsNumber = (money: TypedMoney): number => {
+export const getTypedMoneyAsNumber = (money?: TypedMoney): number => {
+    if(!money){
+        logger.info('No price found for product.');
+        return 0;
+    }
     return money.centAmount / Math.pow(10, money.fractionDigits);
 };
